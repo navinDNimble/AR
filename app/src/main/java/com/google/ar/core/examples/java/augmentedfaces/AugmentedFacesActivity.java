be3876e0296ce359ccc9351e1f3ce2d1833c26e5
navinDNimble <navind@nimblei.com>
2023-12-28 17:37:38 +0530

    Initial Commit 28-12-23

--- app/src/main/java/com/google/ar/core/examples/java/augmentedfaces/AugmentedFacesActivity.java
+++ app/src/main/java/com/google/ar/core/examples/java/augmentedfaces/AugmentedFacesActivity.java
@@ -18,12 +18,10 @@ package com.google.ar.core.examples.java.augmentedfaces;
 
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
-import android.opengl.Matrix;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.ar.core.ArCoreApk;
@@ -53,7 +51,6 @@ import com.google.ar.core.exceptions.UnavailableSdkTooOldException;
 import com.google.ar.core.exceptions.UnavailableUserDeclinedInstallationException;
 
 import java.io.IOException;
-import java.nio.FloatBuffer;
 import java.util.Collection;
 import java.util.EnumSet;
 import java.util.List;
@@ -78,123 +75,49 @@ public class AugmentedFacesActivity extends AppCompatActivity implements GLSurfa
     private final SnackbarHelper messageSnackbarHelper = new SnackbarHelper();
     private DisplayRotationHelper displayRotationHelper;
     private final TrackingStateHelper trackingStateHelper = new TrackingStateHelper(this);
+
     private final BackgroundRenderer backgroundRenderer = new BackgroundRenderer();
     private final AugmentedFaceRenderer augmentedFaceRenderer = new AugmentedFaceRenderer();
     private final ObjectRenderer noseObject = new ObjectRenderer();
-
+    private final ObjectRenderer rightEarObject = new ObjectRenderer();
+    private final ObjectRenderer leftEarObject = new ObjectRenderer();
+    private final ObjectRenderer neckObject = new ObjectRenderer();
+    // Temporary matrix allocated here to reduce number of allocations for each frame.
     private final float[] noseMatrix = new float[16];
-
+    private final float[] rightEarMatrix = new float[16];
+    private final float[] leftEarMatrix = new float[16];
+    private final float[] neckMatrix = new float[16];
     private static final float[] DEFAULT_COLOR = new float[]{0f, 0f, 0f, 0f};
-
-
-    //sachin code
-
-    @Override
-    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
-        GLES20.glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
-
-        try {
-
-            backgroundRenderer.createOnGlThread(this);
-            augmentedFaceRenderer.createOnGlThread(this, "models/freckles.png");
-            augmentedFaceRenderer.setMaterialProperties(0.0f, 1.0f, 0.1f, 6.0f);
-            noseObject.createOnGlThread(/*context=*/ this, "models/neck_less.obj", "models/neckless_img.jpg");
-            noseObject.setMaterialProperties(0.0f, 1.0f, 0.1f, 6.0f);
-            noseObject.setBlendMode(ObjectRenderer.BlendMode.AlphaBlending);
-
-        } catch (IOException e) {
-            Log.e(TAG, "Failed to read an asset file", e);
-        }
-    }
-
-    @Override
-    public void onDrawFrame(GL10 gl) {
-
-        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT | GLES20.GL_DEPTH_BUFFER_BIT);
-
-        if (session == null) {
-            return;
-        }
-
-        displayRotationHelper.updateSessionIfNeeded(session);
-
-        try {
-            session.setCameraTextureName(backgroundRenderer.getTextureId());
-
-            Frame frame = session.update();
-            Camera camera = frame.getCamera();
-
-            float[] projectionMatrix = new float[16];
-            camera.getProjectionMatrix(projectionMatrix, 0, 0.1f, 100.0f);
-
-            float[] viewMatrix = new float[16];
-            camera.getViewMatrix(viewMatrix, 0);
-
-            final float[] colorCorrectionRgba = new float[4];
-            frame.getLightEstimate().getColorCorrection(colorCorrectionRgba, 0);
-
-            backgroundRenderer.draw(frame);
-            trackingStateHelper.updateKeepScreenOnFlag(camera.getTrackingState());
-            Collection<AugmentedFace> faces = session.getAllTrackables(AugmentedFace.class);
-
-            for (AugmentedFace face : faces) {
-                if (face.getTrackingState() != TrackingState.TRACKING) {
-                    break;
-                }
-
-                FloatBuffer faceVertices = face.getMeshVertices();
-                Pose facePose = face.getCenterPose();
-                float lowestY = Float.MAX_VALUE;
-                for (int i = 0; i < faceVertices.capacity(); i += 3) {
-                    float x = faceVertices.get(i);
-                    float y = faceVertices.get(i + 1);
-                    float z = faceVertices.get(i + 2);
-                    float[] vertexWorld = new float[3];
-                    facePose.transformPoint(new float[]{x, y, z}, 0, vertexWorld, 0);
-                    lowestY = Math.min(lowestY, vertexWorld[1]);
-                }
-                float scaleFactor = 1.0f;
-                GLES20.glDepthMask(false);
-                float[] modelMatrix = new float[16];
-                augmentedFaceRenderer.draw(projectionMatrix, viewMatrix, modelMatrix, colorCorrectionRgba, face);
-                face.getCenterPose().toMatrix(noseMatrix, 0);
-                float[] translationMatrix = new float[16];
-                Matrix.setIdentityM(translationMatrix, 0);
-                Matrix.translateM(translationMatrix, 0, 0.0f, lowestY - 0.11f, 0.0f);  // Adjust the Y translation
-                Matrix.multiplyMM(noseMatrix, 0, noseMatrix, 0, translationMatrix, 0);
-                noseObject.updateModelMatrix(noseMatrix, scaleFactor);
-                noseObject.draw(viewMatrix, projectionMatrix, colorCorrectionRgba, DEFAULT_COLOR);
-            }
-        } catch (Throwable t) {
-
-            Log.e(TAG, "Exception on the OpenGL thread", t);
-        } finally {
-            GLES20.glDepthMask(true);
-        }
-    }
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         surfaceView = findViewById(R.id.surfaceview);
-        displayRotationHelper = new DisplayRotationHelper(this);
+        displayRotationHelper = new DisplayRotationHelper(/*context=*/ this);
+
+        // Set up renderer.
         surfaceView.setPreserveEGLContextOnPause(true);
         surfaceView.setEGLContextClientVersion(2);
         surfaceView.setEGLConfigChooser(8, 8, 8, 8, 16, 0); // Alpha used for plane blending.
         surfaceView.setRenderer(this);
         surfaceView.setRenderMode(GLSurfaceView.RENDERMODE_CONTINUOUSLY);
         surfaceView.setWillNotDraw(false);
+
         installRequested = false;
     }
 
     @Override
     protected void onDestroy() {
         if (session != null) {
+            // Explicitly close ARCore Session to release native resources.
+            // Review the API reference for important considerations before calling close() in apps with
+            // more complicated lifecycle requirements:
+            // https://developers.google.com/ar/reference/java/arcore/reference/com/google/ar/core/Session#close()
             session.close();
             session = null;
         }
+
         super.onDestroy();
     }
 
@@ -214,18 +137,21 @@ public class AugmentedFacesActivity extends AppCompatActivity implements GLSurfa
                         break;
                 }
 
+                // ARCore requires camera permissions to operate. If we did not yet obtain runtime
+                // permission on Android M and above, now is a good time to ask the user for it.
                 if (!CameraPermissionHelper.hasCameraPermission(this)) {
                     CameraPermissionHelper.requestCameraPermission(this);
                     return;
                 }
 
-
+                // Create the session and configure it to use a front-facing (selfie) camera.
                 session = new Session(/* context= */ this, EnumSet.noneOf(Session.Feature.class));
                 CameraConfigFilter cameraConfigFilter = new CameraConfigFilter(session);
                 cameraConfigFilter.setFacingDirection(CameraConfig.FacingDirection.FRONT);
                 List<CameraConfig> cameraConfigs = session.getSupportedCameraConfigs(cameraConfigFilter);
                 if (!cameraConfigs.isEmpty()) {
-
+                    // Element 0 contains the camera config that best matches the session feature
+                    // and filter settings.
                     session.setCameraConfig(cameraConfigs.get(0));
                 } else {
                     message = "This device does not have a front-facing (selfie) camera";
@@ -275,7 +201,9 @@ public class AugmentedFacesActivity extends AppCompatActivity implements GLSurfa
     public void onPause() {
         super.onPause();
         if (session != null) {
-
+            // Note that the order matters - GLSurfaceView is paused first so that it does not try
+            // to query the session. If Session is paused before GLSurfaceView, GLSurfaceView may
+            // still call session.update() and get a SessionPausedException.
             displayRotationHelper.onPause();
             surfaceView.onPause();
             session.pause();
@@ -283,13 +211,13 @@ public class AugmentedFacesActivity extends AppCompatActivity implements GLSurfa
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] results) {
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] results) {
         super.onRequestPermissionsResult(requestCode, permissions, results);
         if (!CameraPermissionHelper.hasCameraPermission(this)) {
             Toast.makeText(this, "Camera permission is needed to run this application", Toast.LENGTH_LONG)
                     .show();
             if (!CameraPermissionHelper.shouldShowRequestPermissionRationale(this)) {
-
+                // Permission denied with checking "Do not ask again".
                 CameraPermissionHelper.launchPermissionSettings(this);
             }
             finish();
@@ -302,6 +230,37 @@ public class AugmentedFacesActivity extends AppCompatActivity implements GLSurfa
         FullScreenHelper.setFullScreenOnWindowFocusChanged(this, hasFocus);
     }
 
+    @Override
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+        GLES20.glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+
+        // Prepare the rendering objects. This involves reading shaders, so may throw an IOException.
+        try {
+            // Create the texture and pass it to ARCore session to be filled during update().
+            backgroundRenderer.createOnGlThread(/*context=*/ this);
+//            augmentedFaceRenderer.createOnGlThread(this, "models/freckles.png");
+//            augmentedFaceRenderer.setMaterialProperties(0.0f, 1.0f, 0.1f, 6.0f);
+            /*noseObject.createOnGlThread(*//*context=*//* this, "models/nose.obj", "models/nose_fur.png");
+            noseObject.setMaterialProperties(0.0f, 1.0f, 0.1f, 6.0f);
+            noseObject.setBlendMode(ObjectRenderer.BlendMode.AlphaBlending);
+            rightEarObject.createOnGlThread(this, "models/forehead_right.obj", "models/ear_fur.png");
+            rightEarObject.setMaterialProperties(0.0f, 1.0f, 0.1f, 6.0f);
+            rightEarObject.setBlendMode(ObjectRenderer.BlendMode.AlphaBlending);
+            leftEarObject.createOnGlThread(this, "models/forehead_left.obj", "models/ear_fur.png");
+            leftEarObject.setMaterialProperties(0.0f, 1.0f, 0.1f, 6.0f);
+            leftEarObject.setBlendMode(ObjectRenderer.BlendMode.AlphaBlending);*/
+
+
+            neckObject.createOnGlThread(this, "models/model.obj", "models/neckless1.png");
+            neckObject.setMaterialProperties(0.1f, 1.0f, 0.1f, 0.0f);
+            neckObject.setBlendMode(ObjectRenderer.BlendMode.AlphaBlending);
+
+
+
+        } catch (IOException e) {
+            Log.e(TAG, "Failed to read an asset file", e);
+        }
+    }
 
     @Override
     public void onSurfaceChanged(GL10 gl, int width, int height) {
@@ -309,12 +268,117 @@ public class AugmentedFacesActivity extends AppCompatActivity implements GLSurfa
         GLES20.glViewport(0, 0, width, height);
     }
 
+    @Override
+    public void onDrawFrame(GL10 gl) {
+        // Clear screen to notify driver it should not load any pixels from previous frame.
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT | GLES20.GL_DEPTH_BUFFER_BIT);
+
+        if (session == null) {
+            return;
+        }
+        // Notify ARCore session that the view size changed so that the perspective matrix and
+        // the video background can be properly adjusted.
+        displayRotationHelper.updateSessionIfNeeded(session);
+
+        try {
+            session.setCameraTextureName(backgroundRenderer.getTextureId());
+
+            // Obtain the current frame from ARSession. When the configuration is set to
+            // UpdateMode.BLOCKING (it is by default), this will throttle the rendering to the
+            // camera framerate.
+            Frame frame = session.update();
+            Camera camera = frame.getCamera();
+
+            // Get projection matrix.
+            float[] projectionMatrix = new float[16];
+            camera.getProjectionMatrix(projectionMatrix, 0, 0.1f, 100.0f);
+
+            // Get camera matrix and draw.
+            float[] viewMatrix = new float[16];
+            camera.getViewMatrix(viewMatrix, 0);
+
+            // Compute lighting from average intensity of the image.
+            // The first three components are color scaling factors.
+            // The last one is the average pixel intensity in gamma space.
+            final float[] colorCorrectionRgba = new float[4];
+            frame.getLightEstimate().getColorCorrection(colorCorrectionRgba, 0);
+
+            // If frame is ready, render camera preview image to the GL surface.
+            backgroundRenderer.draw(frame);
+
+            // Keep the screen unlocked while tracking, but allow it to lock when tracking stops.
+            trackingStateHelper.updateKeepScreenOnFlag(camera.getTrackingState());
+
+            // ARCore's face detection works best on upright faces, relative to gravity.
+            // If the device cannot determine a screen side aligned with gravity, face
+            // detection may not work optimally.
+            Collection<AugmentedFace> faces = session.getAllTrackables(AugmentedFace.class);
+            for (AugmentedFace face : faces) {
+                if (face.getTrackingState() != TrackingState.TRACKING) {
+                    break;
+                }
+
+                float scaleFactor = 1.0f;
+
+                // Render the neck object attached to the head.
+                float[] neckMatrix = new float[16];
+                face.getCenterPose().toMatrix(neckMatrix, 0);
+                neckObject.updateModelMatrix(neckMatrix, scaleFactor);
+                neckObject.draw(viewMatrix, projectionMatrix, colorCorrectionRgba, DEFAULT_COLOR);
+
+
+                // Face objects use transparency so they must be rendered back to front without depth write.
+                GLES20.glDepthMask(false);
+
+                // Each face's region poses, mesh vertices, and mesh normals are updated every frame.
+
+                // 1. Render the face mesh first, behind any 3D objects attached to the face regions.
+                float[] modelMatrix = new float[16];
+                face.getCenterPose().toMatrix(modelMatrix, 0);
+                augmentedFaceRenderer.draw(
+                        projectionMatrix, viewMatrix, modelMatrix, colorCorrectionRgba, face);
+
+                // 2. Next, render the 3D objects attached to the forehead.
+                face.getRegionPose(RegionType.FOREHEAD_RIGHT).toMatrix(rightEarMatrix, 0);
+                rightEarObject.updateModelMatrix(rightEarMatrix, scaleFactor);
+                rightEarObject.draw(viewMatrix, projectionMatrix, colorCorrectionRgba, DEFAULT_COLOR);
+
+                face.getRegionPose(RegionType.FOREHEAD_LEFT).toMatrix(leftEarMatrix, 0);
+                leftEarObject.updateModelMatrix(leftEarMatrix, scaleFactor);
+                leftEarObject.draw(viewMatrix, projectionMatrix, colorCorrectionRgba, DEFAULT_COLOR);
+
+                // 3. Render the nose last so that it is not occluded by face mesh or by 3D objects attached
+                // to the forehead regions.
+                face.getRegionPose(RegionType.NOSE_TIP).toMatrix(noseMatrix, 0);
+                noseObject.updateModelMatrix(noseMatrix, scaleFactor);
+                noseObject.draw(viewMatrix, projectionMatrix, colorCorrectionRgba, DEFAULT_COLOR);
+
+
+            }
+        } catch (Throwable t) {
+            // Avoid crashing the application due to unhandled exceptions.
+            Log.e(TAG, "Exception on the OpenGL thread", t);
+        } finally {
+            GLES20.glDepthMask(true);
+        }
+    }
+
+    private float[] estimateNeckPosition(Pose regionPose, Pose regionPose1, Pose regionPose2) {
+        float[] neckPosition = new float[16];
+
+        // Example: Simple averaging of the forehead landmarks.
+        for (int i = 0; i < 16; i++) {
+            neckPosition[i] = (regionPose.getTranslation()[i] +
+                    regionPose1.getTranslation()[i] +
+                    regionPose2.getTranslation()[i]) / 3.0f;
+        }
+
+        return neckPosition;
+    }
 
     private void configureSession() {
         Config config = new Config(session);
         config.setAugmentedFaceMode(AugmentedFaceMode.MESH3D);
         session.configure(config);
-
-
     }
 }
